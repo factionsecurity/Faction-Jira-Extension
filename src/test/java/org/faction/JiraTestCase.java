9f6dd4d80554efd2bf495c26811e4f9ea33dacd5
Josh <ascetik@gmail.com>
2024-03-02 16:48:52 -0600

    update tests and pom.xml

--- src/test/java/org/faction/JiraTestCase.java
+++ src/test/java/org/faction/JiraTestCase.java
@@ -29,34 +29,5 @@ class JiraTestCase {
 		assertTrue(jiraIssueId.matches("[0-9]+"));
 		
 	}
-	
-	@Test
-	void loadExtension() throws MalformedURLException {
-		Vulnerability vuln = new Vulnerability();
-		vuln.setDescription("test issue");
-		vuln.setName("Faction Vulnerability");
-		List<Vulnerability>vulns = new ArrayList<>();
-		vulns.add(vuln);
-		Assessment asmt = new Assessment();
-		asmt.setName("Test Assessment");
-		File f = new File("/opt/faction/modules/JiraPlugin-0.0.1-jar-with-dependencies.jar");
-		URL [] urls = {f.toURL()};
-		URLClassLoader extensionClassLoader = new URLClassLoader(urls);
-		
-		ClassLoader currentClassLoader = Thread.currentThread().getContextClassLoader();
-		try {
-		      Thread.currentThread().setContextClassLoader(extensionClassLoader);
-		      for (AssessmentManager extension : ServiceLoader.load(AssessmentManager.class,extensionClassLoader)) {
-		    	  Object [] output = extension.assessmentChange(asmt, vulns, Operation.Finalize);
-		    	  assertTrue(output != null);
-		    	  assertTrue(output.length == 2);
-		      }
-		} catch (Throwable ex) {
-			ex.printStackTrace();
-		} finally {
-		  Thread.currentThread().setContextClassLoader(currentClassLoader);
-		}
-		
-	}
 
 }
